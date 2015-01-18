9afd93068a778ffd4788229ad0e52cb6d8fed855
kurooooon <b1006110@gmail.com>
2015-01-19 00:28:08 +0900

    あまりにも安定しないのでキャラを四角に変更 移動時にCharacterControllerは使用せずPlayerControllerのscriptで移動

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -6,43 +6,33 @@ using System.Collections;
 /// </summary>
 public class PlayerController : MonoBehaviour {
 
-	private GameObject _player;
-	private CharacterController _cController;
+	public float moveSpeed = 10.0f;
 
-	private float _speed = 10.0f;
-	private float _gravity = 20.0f;
+	private GameObject _player;
+//	private CharacterController _cController;
+//	private float _gravity = 20.0f;
 	private Vector3 _moveDirection;
 
 	// Use this for initialization
 	void Start () {
 		_player = GameObject.Find (GameConfig.CODE_PLAYER);
-		_cController = GetComponent<CharacterController>();
+//		_cController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		//移動方向を取得
-		_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		_moveDirection = transform.TransformDirection(_moveDirection);
-		_moveDirection *= _speed;
 
-		// 移動
-		_cController.Move(_moveDirection * Time.deltaTime);
+		// 右・左
+		float x = Input.GetAxisRaw ("Horizontal");
 
-//		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-//			print ("space key was pressed");
-//		}
-//		if (Input.GetKeyDown (KeyCode.DownArrow)) {
-//
-//			print ("space key was pressed");
-//		}
-//		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-//			print ("space key was pressed");
-//		}
-//		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-//			print ("space key was pressed");
-//		}
-	
+		// 上・下
+		float z = Input.GetAxisRaw ("Vertical");
+
+		// 移動する向きを求める
+		Vector3 direction = new Vector3 (x, 0, z).normalized;
+		direction.y = -0.9f;
+
+		// 移動する向きとスピードを代入する
+		rigidbody.velocity = direction * moveSpeed;
 	}
 }
